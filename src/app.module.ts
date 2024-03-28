8a28f0e9390aeaf90ebd706fb3d0e710c22a2444
anna <anna.pushkareva@smddev.com>
2024-03-28 17:49:38 +0300

    add track module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [TrackModule, MongooseModule.forRoot(process.env.DB_URL)],
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-    getUsers() {
-        return 'GET ALL USERS';
-    }
-}
