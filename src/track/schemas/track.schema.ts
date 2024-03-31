c66cc498d70fd872fb0cd513aa1193c0a755b234
Anna Pushkareva <smirnova_spb@mail.ru>
2024-03-31 10:02:07 +0300

    add comment schema, track dto,

--- src/track/schemas/track.schema.ts
+++ src/track/schemas/track.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { Comment } from './comment.schema';
 
 export type TrackDocument = HydratedDocument<Track>;
 
@@ -22,6 +24,9 @@ export class Track {
 
     @Prop()
     audio: string;
+
+    @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }] })
+    comments: Comment[];
 }
 
 export const TrackSchema = SchemaFactory.createForClass(Track);
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Track, TrackDocument } from './schemas/track.schema';
+import { Model } from 'mongoose';
+import { CommentDocument } from './schemas/comment.schema';
+import { CreateTrackDto } from './dto/create-track.dto';
 
 @Injectable()
 export class TrackService {
-    async create() {}
+    constructor(
+        @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+        @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+    ) {}
+    async create(dto: CreateTrackDto): Promise<Track> {
+        const track = await this.trackModel.create({ ...dto, listens: 0 });
+        return track;
+    }
 
     async getAll() {}
 
